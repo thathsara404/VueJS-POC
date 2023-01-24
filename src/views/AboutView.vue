43f266ab97aca07154b19df9c2149251285bf0a3
thathsara404 <you@example.com>
2023-01-25 01:06:41 +0530

    add initial files

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button class="btn btn-primary" data-bs-target="#collapseTarget" data-bs-toggle="collapse">
+      Bootstrap collapse
+    </button>
+    <div class="collapse py-2" id="collapseTarget">This is the toggle-able content!</div>
+  </div>
+</template>
