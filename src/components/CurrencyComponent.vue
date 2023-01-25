3c6c05b98a179ad8a8817e7910b8291116c29f36
thathsara404 <you@example.com>
2023-01-25 20:20:27 +0530

    add v-for and v-if and directive

---
+++ src/components/CurrencyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>
+    {{ convertToDoller(amount) }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: ['amount'],
+  data() {
+    return {
+      rate: 370
+    };
+  },
+  methods: {
+    convertToDoller(value) {
+      return Number.parseFloat(value).toFixed(2);
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="jumbotron">
+      <h1 class="display-4">Simple Converter</h1>
+      <p class="lead">This is a simple money converter app</p>
+      <img :src="imgMoneySrc" :alt="imgMoneyAlt" />
+      <hr class="my-4" />
+      <h3>Points to remember</h3>
+      <ul>
+        <template v-for="(item, index) in importantPoints" :key="index">
+          <li v-if="item.point !== 'Test'" :key="index">{{ item.point }} - {{ item.details }}</li>
+        </template>
+      </ul>
+    </div>
+    <currency-component amount="10" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import CurrencyComponent from '@/components/CurrencyComponent.vue';
+import imgMoney from '@/assets/imagemoney.png';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
+    CurrencyComponent
   },
+  mounted() {
+    setTimeout(() => {
+      // Check, v-for support for array mutation. When mutation happen,
+      // for loop will trigger automatically
+      this.importantPoints[0].point = 'Authenticity';
+    }, 5000);
+  },
+  data() {
+    return {
+      imgMoneySrc: imgMoney,
+      imgMoneyAlt: 'This is a money image',
+      importantPoints: [
+        {
+          point: 'Security',
+          details: 'This is a secure app to conert your money.'
+        },
+        {
+          point: 'Latest Rate',
+          details: 'You will see the realtime rates here.'
+        },
+        {
+          point: 'Test',
+          details: 'This is for testing.'
+        }
+      ]
+    };
+  }
 };
 </script>
