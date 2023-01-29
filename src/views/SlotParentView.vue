7542d07559769da5bad447082778b2156526cfa5
thathsara404 <thathsararaviraj@gmail.com>
2023-01-29 18:27:33 +0530

    add slot implementation

---
+++ src/views/SlotParentView.vue
@@ -0,0 +1,60 @@
+<template>
+  <h3>Parent View</h3>
+  <div class="row">
+    <div class="col-md-12">
+      <group-slot-component>
+        <template #header>Header Text</template>
+        <template #default></template>
+        <template #footer>Footer Text</template>
+      </group-slot-component>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <label for="dataSelect">Select Data
+        <select v-model="dataSelect" id="dataSelect" class="form-control">
+          <option disabled>Choose...</option>
+          <option value="name">Name</option>
+          <option value="address">Address</option>
+        </select>
+      </label><br><br>
+    </div>
+  </div>
+  <div class="row">
+    <!-- ACCESS SLOT DATA OF A CHILD SLOT COMPONENT BASED ON THE PARENT REQUIREMENT-->
+    <slot-component>
+      <template v-slot="slotData">
+        <div v-if="dataSelect === 'name'">
+          {{ slotData.myNameSlot }}
+        </div>
+        <div v-else-if="dataSelect === 'address'">
+          {{ slotData.myAddressSlot }}
+        </div>
+      </template>
+    </slot-component>
+  </div>
+</template>
+
+<script>
+import SlotComponent from '@/components/SlotComponent.vue';
+import GroupSlotComponent from '@/components/GroupSlotComponent.vue';
+
+export default {
+  name: 'CustomVModelView',
+  props: [],
+  components: { SlotComponent, GroupSlotComponent },
+  data() {
+    return {
+      dataSelect: ''
+    };
+  },
+  methods: {
+
+  }
+
+};
+</script>
+
+<style>
+
+</style>
